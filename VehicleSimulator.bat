2f4995137e1d06f51f9729f39a10c054df0c8a82
calimer <platinumarts@gmail.com>
2014-05-10 13:15:42 -0400

    Update VehicleSimulator.bat
    Cleaned up bad .bat code

--- VehicleSimulator.bat
+++ VehicleSimulator.bat
@@ -1,3 +1,1 @@
-<<<<<<< .mine
-bin\sandbox_krs.exe -r -lautodrome -qmy_stuff %*=======
-start bin\sandbox_krs.exe -qmy_stuff -r -gstdout.txt %*>>>>>>> .r3395
+bin\sandbox_krs.exe -r -lautodrome -qmy_stuff -gstdout.txt %1 %2 %3 %4 %5
